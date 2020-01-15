f93a4b7991fe3afd6bd02397f2080592d46207d3
Daniel Succar <danielsuccar@icloud.com>
2020-01-15 19:13:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,6 +10,10 @@ end
 introduction_with_language("Dan", "Ember.js")
 introduction_with_language("Josh", "React")
 
+def introduction_with_language_optional(name, language = Ruby)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
+
 
 
 
